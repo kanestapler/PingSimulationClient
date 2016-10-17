e88a5e5a01b4818d1544bba49ccc595f2a326abf
kanestapler <kanestapler@gmail.com>
2016-10-17 14:42:33 -0500

    Changing from TCP to UDP

--- src/PingThread.java
+++ src/PingThread.java
@@ -16,38 +16,4 @@ public class PingThread extends Thread {
         }catch (InterruptedException e) { }
         System.out.println("Stopping Thread Number: " + threadNum);
     }
-    
-    public void Test() {
-    	
-    	InputStreamReader in = new InputStreamReader(System.in);
-    	BufferedReader bufR = new BufferedReader(in);
-    	String message = "";
-		while (true) {
-			try {
-				//create a socket and connect
-				Socket socket = new Socket("localhost", 4400);
-				System.out.print("Enter a MESSAGE: ");
-				message = bufR.readLine();
-	
-				socket.setSendBufferSize(message.length());
-	
-				//connect to a buffered reader
-				BufferedOutputStream bos = new
-						BufferedOutputStream(socket.getOutputStream(),
-								socket.getSendBufferSize());
-				for (int i=0; i<message.length(); i++){
-					bos.write((int) message.charAt(i));}
-	
-				bos.close();
-				if (message.equals("bye")){
-					System.out.println("Closing.... goodbye!");
-					break;
-				}
-	
-				socket.close();
-			} catch (Exception e) {
-				System.out.println("Error " + e.getMessage());
-			}
-		}
-    }
 }
